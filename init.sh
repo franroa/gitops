bcc9809f759ec419f1dac2ecbee90f4a4e25466d
franroasynaos <francisco.roa.prieto@synaos.com>
2020-12-25 05:49:36 -0800

    test

--- init.sh
+++ init.sh
@@ -3,15 +3,19 @@
 k3d cluster delete fran
 k3d cluster create fran
 
-fluxctl install \
-  --namespace flux \
-  --git-url "git@github.com:franroa/gitops.git" \
-  --git-user franroa \
-  --git-email "franroa@users.noreply.github.com" | kubectl apply -f -
+
+# Working without helm releases
+#fluxctl install \
+#  --namespace flux \
+#  --git-url "git@github.com:franroa/gitops.git" \
+#  --git-user franroa \
+#  --git-email "franroa@users.noreply.github.com" | kubectl apply -f -
 
 
 # TODO -> Sign commits with GPG
 
+
+kubectl create ns flux
 
 kubectl apply -f https://raw.githubusercontent.com/fluxcd/helm-operator/1.2.0/deploy/crds.yaml
 
